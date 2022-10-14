a45d158bcc26aacef23fba2c4bac1915490d3954
patcty <51163994+patcty@users.noreply.github.com>
2022-10-14 12:41:10 +0800

    Day 1 Azure workshop commit

---
+++ Day1/demostorageapp/Program.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Storage;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace demostorageblob
+{
+    public class Program
+    {
+        private const string STORAGECONNSTRING = "{replace with your storage connection string}";
+        private const string CONTAINERNAME = "original-image";
+        
+        static void Main(string[] args)
+        {
+            UploadFile();
+        }
+
+        private static void UploadFile()
+        {
+            string blobName = "{the name of the blob you want to upload to}";
+            string filePath = @"{the path to the file and this should include the file}";
+
+            try{
+                // Get a reference to the container
+                BlobContainerClient container = new BlobContainerClient(STORAGECONNSTRING, CONTAINERNAME);
+                container.CreateIfNotExists();
+
+                // Get a reference to a blob. The blob name can be anything but as a good practice, make it the same name as the object you want to upload
+                BlobClient blob = container.GetBlobClient(blobName);
+
+                // Upload local file
+                blob.Upload(filePath);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
---
+++ Day1/demostoragequeue/Program.cs
@@ -0,0 +1,83 @@
+﻿using Azure.Storage;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using Azure.Data;
+using Azure.Data.Tables;
+using Azure.Data.Tables.Models;
+using Azure;
+
+namespace demostoragequeue
+{
+    public class Program
+    {
+        private const string STORAGECONNSTRING = "{replace with your storage connection string}";
+        private const string QUEUENAME = "message-queue";
+
+        static void Main(string[] args)
+        {
+            WriteSomethingToQueue();
+
+            ProcessQueue();
+        }
+
+// Azure.Storage.Queues
+// Azure.Data.Tables
+        private static void WriteSomethingToQueue()
+        {
+            try{
+                QueueClient client = new QueueClient(STORAGECONNSTRING, QUEUENAME);
+                client.CreateIfNotExists();
+
+                client.SendMessage("Message 1 to the queue");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private static void ProcessQueue()
+        {
+            try{
+                // Read from the queue and write it to the table storage
+                QueueClient client = new QueueClient(STORAGECONNSTRING, QUEUENAME);
+
+                foreach(var message in client.ReceiveMessages(maxMessages: 5).Value)
+                {
+                    Console.WriteLine($"Message: {message.Body}");
+
+                    WriteMessageToTable(message);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private static void WriteMessageToTable(QueueMessage message)
+        {
+            try{
+                var tableServiceClient = new TableServiceClient(STORAGECONNSTRING);
+
+                string partitionKey = "message-partition";
+                string rowKey = Guid.NewGuid().ToString();
+
+                TableItem table = tableServiceClient.CreateTableIfNotExists("demomessageTable");
+
+                var tableClient = tableServiceClient.GetTableClient(tableName: "demomessageTable");
+                tableClient.CreateIfNotExists();
+
+                var entity = new TableEntity(partitionKey, rowKey){
+                    {"TheMessage", message.Body.ToString()}
+                };
+
+                tableClient.AddEntity(entity);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
